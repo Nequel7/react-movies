beddfb383fa580c702f4c11e6f95110a07b1fc7d
Ivan Zubrik <zubria2003@gmail.com>
2024-08-28 19:16:45 +0300

    fix mixed content

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,7 +3,7 @@ import {Movies} from '../components/Movies';
 import {Preloader} from '../components/Preloader';
 import {Search} from '../components/Search';
 
-const API_KEY=process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Main extends React.Component {
     state = {
@@ -12,20 +12,26 @@ class Main extends React.Component {
     }
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=Breaking+Bad`)
+        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=Breaking+Bad`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data => this.setState({movies: data.Search, loading: false})).catch((err) => {
+            console.error(err);
+            this.setState({loading: false})
+        })
     }
 
     searchMovies = (movie, type = 'all') => {
-        this.setState({loading:true})
-        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${movie}${type !== 'all' ? `&type=${type}` : ''}`)
+        this.setState({loading: true})
+        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${movie}${type !== 'all' ? `&type=${type}` : ''}`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data => this.setState({movies: data.Search, loading: false})).catch((err) => {
+            console.error(err);
+            this.setState({loading: false})
+        })
     }
 
     render() {
-        const {movies,loading} = this.state;
+        const {movies, loading} = this.state;
 
         return <main className='container content'>
             <Search searchMovies={this.searchMovies}/>
