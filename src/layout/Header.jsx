6782b4ef008ea1ca405294c6d783e5c331bc4c0c
Ivan Zubrik <zubria2003@gmail.com>
2024-08-28 19:26:40 +0300

    fix redir urls

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav className='deep-purple darken-3'>
         <div className="nav-wrapper">
-            <a href="src/layout#" className="brand-logo">Nequel Movies</a>
+            <a href="https://nequel7.github.io/react-movies" className="brand-logo">Nequel Movies</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#">Repo</a></li>
+                <li><a href="https://github.com/Nequel7/react-movies">Repo</a></li>
 
             </ul>
         </div>
