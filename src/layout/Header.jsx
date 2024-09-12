81bf83bfce15dc5c61e734a74b6ceb4fbfeb97f0
Ivan Zubrik <zubria2003@gmail.com>
2024-09-13 02:12:58 +0300

    upd repo url

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <div className="nav-wrapper">
             <a href="https://nequel7.github.io/react-movies" className="brand-logo">Nequel Movies</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="https://github.com/Nequel7/react-movies">Repo</a></li>
+                <li><a href="https://github.com/Nequel7/react-movies" target="_blank" rel="noreferrer">Repo</a></li>
 
             </ul>
         </div>
