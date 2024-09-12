89f6c4f35703ae3524e4dee839c9c4ce2c6445ed
Ivan Zubrik <zubria2003@gmail.com>
2024-09-13 02:14:36 +0300

    upd repo url

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return <nav className='deep-purple darken-3'>
         <div className="nav-wrapper">
-            <a href="https://nequel7.github.io/react-movies" className="brand-logo" target="_blank" rel="noreferrer">Nequel Movies</a>
+            <a href="https://nequel7.github.io/react-movies" className="brand-logo">Nequel Movies</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="https://github.com/Nequel7/react-movies" target="_blank" rel="noreferrer">Repo</a></li>
 
